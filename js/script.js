757f6117f93fdb0b1b8ffbd540414f00295eadef
Tommaso P <tommaso.plinio@gmail.com>
2021-03-17 16:00:16 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// 1 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+
+var studente1 = {
+  'nome' : 'Luca',
+  'cognome': 'Rossi',
+  'età' : 25
+}
